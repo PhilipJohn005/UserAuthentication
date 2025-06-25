c5a0e6905d6a8fa2da61520c3388bb8b892497c5
Philip John <160749216+PhilipJohn005@users.noreply.github.com>
2025-06-25 21:12:48 +0530

    Update FirebaseConfig.ts

--- src/Firebase/FirebaseConfig.ts
+++ src/Firebase/FirebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEafXHKHO4sZy3AHbr6SpLQU1JS5YcX_I",
-  authDomain: "crypto-cb142.firebaseapp.com",
-  projectId: "crypto-cb142",
-  storageBucket: "crypto-cb142.firebasestorage.app",
-  messagingSenderId: "690963773596",
-  appId: "1:690963773596:web:dcc8934c6d6291006fd559"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
